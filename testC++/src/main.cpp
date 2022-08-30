6a5c315b106f10cc7e8165865a91252154e38f8c
Brostagma <eminberoglu@gmail.com>
2022-08-30 23:15:31 +0300

    Basic Map Generator

--- testC++/src/main.cpp
+++ testC++/src/main.cpp
@@ -129,34 +129,82 @@ int main() {
     }
     printf("\n");
     printf("\n");
-
-    int mapBasicWorld[8][8] = {{0,0,0,0,0,2,0,0}
-                            ,{0,0,0,0,2,0,0,0}
-                            ,{0,0,0,2,0,0,0,0}
-                            ,{0,0,2,0,0,0,0,0}
-                            ,{1,1,1,0,0,1,1,1}
-                            ,{1,1,1,1,0,1,1,1}
-                            ,{1,3,1,1,1,1,1,1}
-                            ,{1,1,1,1,1,1,1,1}};
-    
-    for(int i = 0; i < 8; i++){
-        for(int j = 0; j < 8; j++){
+    long mapSizeY = 16;
+    long mapSizeX = 16;
+    int mapBasicWorld[mapSizeY][mapSizeX];
+    /*
+                        {{0,0,0,0,0,2,2,2,0,0,0,0,0,1,1,1}
+                        ,{0,0,0,0,2,2,2,0,0,0,0,0,1,1,1,1}
+                        ,{0,0,0,2,2,2,0,0,0,0,0,0,1,1,1,1}
+                        ,{0,0,2,2,2,0,0,0,0,0,1,1,1,1,1,1}
+                        ,{1,1,1,1,1,0,0,0,1,1,1,1,3,1,1,1}
+                        ,{1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1}
+                        ,{1,3,1,1,1,1,1,1,1,3,1,1,1,1,1,1}
+                        ,{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}      EXAMPLE!!!!
+                        ,{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+                        ,{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+                        ,{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+                        ,{1,1,1,1,1,3,1,3,1,1,1,1,1,1,1,1}
+                        ,{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+                        ,{1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1}
+                        ,{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+                        ,{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
+    */
+   srand(time(0));
+    bool isBiom = false;
+    int sizeBiom = 10;
+    int possibilityBiom = 1;
+    int count = 0;
+    cout << "If you enter two maximum variables, there will be no water on the map! \n";
+    cout << "Biom Size: \n (min=0 max=16 default=10) \n (Big number = big biom, low number = small biom) \n";
+    cout << "Enter: ";
+    cin >> sizeBiom;
+    cout << "Biom Possibility: \n (min=0 max=10 default=1) \n (Big number = low water, low number = high water) \n";
+    cout << "Enter: ";
+    cin >> possibilityBiom;
+   for(int z = 0; z < mapSizeY; z++){
+        for(int e = 0; e < mapSizeX; e++){
+            int mapGenerator;
+            if(!isBiom){
+                mapGenerator = (rand() % 10) + 1;
+                if(mapGenerator <= possibilityBiom)
+                    isBiom = true;
+            }
+            if(isBiom){                                 //BASIC MAP GENERATOR
+                count++;
+                mapBasicWorld[z][e] = 1;
+                if(count == sizeBiom){
+                    isBiom = false;
+                    count = 0;
+                }
+                    
+            }else{
+                mapBasicWorld[z][e] = 0;
+            }
+            
+        }
+   }
+    for(int i = 0; i < 16; i++){
+        for(int j = 0; j < 16; j++){
             switch(mapBasicWorld[i][j]){
                 case 0:
                     cout<<"~"; //WATER
                     break;
                 case 1:
-                    cout<<"|"; //ROAD
+                    cout<<"|"; //DIRT
                     break;
+                    /*
                 case 2:
                     cout<<"/"; //HORIZONTAL ROAD
                     break; 
                 case 3:
                     cout<<"v"; //VILLAGE
                     break;
+                    */
             }
         }
         cout<<'\n';
     }
+    cout << "Map Size: " << mapSizeX << "*" << mapSizeY << " = " << (mapSizeX * mapSizeY) <<endl;
     return 0;
 }
