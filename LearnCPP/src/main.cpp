c96916caf5d0506504390e6217922c3a73573023
brostagma <eminberoglu@gmail.com>
2023-03-09 14:25:31 +0300

    .

---
+++ LearnCPP/src/main.cpp
@@ -0,0 +1,26 @@
+#include "add.h"
+#include <iostream>
+
+using namespace std;
+
+void early_test();
+
+int main()
+{
+
+    early_test();
+
+    std::cout << "The sum of 3 and 4 is: " << add(3 ,4) << '\n';
+
+    return 0;
+}
+
+
+void early_test()
+{
+    std::cout << "A";
+
+    return;
+
+    std::cout << "B";
+}
